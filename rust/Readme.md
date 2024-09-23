85e555e2b0f415686e03117c1fea1acf60e6fd62
Phylliida <phylliida.dev@gmail.com>
2024-09-23 13:09:51 -0400

    working, not multithreaded but that's ok

--- rust/Readme.md
+++ rust/Readme.md
@@ -2,3 +2,4 @@ rustup target add wasm32-wasip1
  wasm-objdump target/wasm32-wasip1/debug/tokio-wasi.wasm -j import -x
 rustup install nightly 
  rustup target add wasm32-wasip1 --toolchain nightly
+wasmtime --wasi threads --invoke main target/wasm32-wasip1-threads/debug/example_tokio_wasm.wasm
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,13 +2,30 @@
 
 use tokio;
 
+use std::{thread, time::Duration};
+
+
 #[no_mangle]
-pub async extern "C" fn _start() {
+pub extern "C" fn main() {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(async { async_fn().await });
+}
+
+// these all execute in sequence (not as seperate threads) because tokio doesn't yet support "rt-multi-thread" 
+async fn async_fn() {
     let mut handles = vec![];
 
     for i in 0..5 {
         let handle = tokio::spawn(async move {
             println!("Task {} is running!", i);
+
+            thread::sleep(Duration::from_millis(1000));
+            println!("Task {} is running!", i);
+
         });
         handles.push(handle);
     }
